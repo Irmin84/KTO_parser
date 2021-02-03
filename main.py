aa44cbd92805b7bc94ee0a100b56696cecff18da
Михаил Tkachev <tipabest@yandex.ru>
2021-02-04 01:17:49 +0300

    Парсер для одного файла готов

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from openpyxl import load_workbook
+import openpyxl
 
+# В значениях словарей уазаны номера столбцов, для считывания значений (начиная с 0 для екселя)
 search_scheme_title_page = {
     'Номер объекта:': 1,
     'Наименование:': 1,
@@ -20,33 +22,83 @@ search_scheme_EPU = {
     'Сумма номинальных емкостей АКБ, Ач:': [5],
     'Вывод': [1, 4],
     'Время автономной работы ориентировочно': [3],
+    'Время автономной работы ориентировочно:': [3],
     'Расчетное время на АКБ:': [3]
     }
 
-TEST_CELL = 'G17'   # Если в ячейки есть данные то лист ексел нужно считать (только для ЭПУ)
+table_header = [
+    'Номер объекта',
+    'Наименование',
+    'Адрес',
+    'Дата проведения работ',
+    'Исполнитель 1',
+    'Тип ЭПУ',
+    'Тип нагрузки',
+    'Выходной ток (общий), А',
+    'Состояние ЭПУ',
+    'Количество групп АКБ',
+    'Количество АКБ в группе',
+    'Тип АКБ (группа 1)',
+    'Тип АКБ (группа 2)',
+    'Тип АКБ (группа 3)',
+    'Тип АКБ (группа 4)',
+    'Всего АКБ в данном ЭПУ',
+    'Сумма номинальных емкостей АКБ, А/ч:',
+    'Вывод',
+    'Количество (Вывод)',
+    'Время автономной работы (ориентировочно)',
+    'Расчетное время на АКБ:'
+    ]
 
-wb = load_workbook('./25001066368-2020-COMPLEX-1.xlsm')
+TEST_CELL = 'G17'   # Если в ячейки есть данные то лист ексел нужно считать (только для ЭПУ)
+titl_data_for_write = []
+epu_data_for_write = []
+end_row_number = 1
+my_wb = None
+my_sheet = None
+
+wb = load_workbook('./25001066368-2020-COMPLEX-1.xlsm', data_only=True)
 # wb = load_workbook('./test.xlsx')
 sh_names = wb.sheetnames
 
-# Читаю титульный лист
-# ws = wb[sh_names[0]]
-# for index, row in enumerate(ws.iter_rows()):
-#     val = row[0].value
-#     if val is None:
-#         continue
-#     for key, shift in search_scheme_title_page.items():
-#         if val == key:
-#             print(f'{row[0].value} {row[shift].value}')
-#             break
 
-# Читаю очет ЭПУ
+# Запись в файл ttps://pythononline.ru/question/chtenie-i-zapis-v-fayl-excel-s-ispolzovaniem-modulya-python-openpyxl
+def write_row_to_file(data_for_write):
+    global end_row_number, my_wb, my_sheet
+    if my_wb is None:
+        my_wb = openpyxl.Workbook()
+        my_sheet = my_wb.active
+        my_sheet.title = "Данные КТО"
+        for column_number in range(len(table_header)):
+            write_cell = my_sheet.cell(row=end_row_number, column=column_number + 1)
+            write_cell.value = table_header[column_number]
+        end_row_number += 1
+
+    for column_number in range(len(data_for_write)):
+        write_cell = my_sheet.cell(row=end_row_number, column=column_number + 1)
+        write_cell.value = data_for_write[column_number]
+    end_row_number += 1
+    my_wb.save("./Book1.xlsx")
+
+# Читаю титульный лист
+ws = wb[sh_names[0]]
+for index, row in enumerate(ws.iter_rows()):
+    val = row[0].value
+    if val is None:
+        continue
+    for key, shift in search_scheme_title_page.items():
+        if val == key:
+            # print(f'{row[0].value} {row[shift].value}')
+            titl_data_for_write.append(row[shift].value)
+            break
+
+# Читаю отчет ЭПУ
 for sh_name in sh_names:
     if sh_name[:3] == 'ЭПУ':
         ws = wb[sh_name]
-        print(f'==========={sh_name}============')
         if ws[TEST_CELL].value is None:
             continue
+        print(f'==========={sh_name}============')
         for index, row in enumerate(ws.iter_rows()):
             val = row[0].value
             if val is None:
@@ -55,6 +107,11 @@ for sh_name in sh_names:
                 if val == key:
                     for shift in shifts:
                         print(f'{row[0].value} {row[shift].value}')
+                        epu_data_for_write.append(row[shift].value)
                     break
+        write_row = titl_data_for_write + epu_data_for_write
+        write_row_to_file(data_for_write=write_row)
+        epu_data_for_write = []
+titl_data_for_write = []
 
-# print(sh_names)
+
